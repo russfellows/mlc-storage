d18a468d136fde60b5825eb7bf6ce3067060e263
Pablo Gonzalez <pablo.gonzalez@factored.ai>
2026-02-06 13:56:20 -0500

    Add constants and configuration

--- mlpstorage/submission_checker/configuration/configuration.py
+++ mlpstorage/submission_checker/configuration/configuration.py
@@ -1,4 +1,29 @@
+from ..constants import *
 
 class Config:
-    def __init__(self):
-        pass
+    def __init__(self, version, submitters):
+        self.version = version
+        self.submitters = submitters
+        
+    def check_submitter(self, submitter):
+        if self.submitters is None:
+            return True
+        return submitter in self.submitters
+    
+    def get_datagen_required_files(self):
+        return DATAGEN_REQUIRED_FILES[self.version]
+    
+    def get_run_required_files(self):
+        return RUN_REQUIRED_FILES[self.version]
+    
+    def get_checkpoint_required_files(self):
+        return CHECKPOINT_REQUIRED_FILES[self.version]
+    
+    def get_datagen_required_folders(self):
+        return DATAGEN_REQUIRED_FOLDERS[self.version]
+    
+    def get_run_required_folders(self):
+        return RUN_REQUIRED_FOLDERS[self.version]
+    
+    def get_checkpoint_required_folders(self):
+        return CHECKPOINT_REQUIRED_FOLDERS[self.version]
--- mlpstorage/submission_checker/constants.py
+++ mlpstorage/submission_checker/constants.py
@@ -1,9 +1,54 @@
+from .parsers.json_parser import JSONParser
+from .parsers.yaml_parser import YamlParser
 
 VERSIONS = ["v2.0", "v3.0"]
 VALID_DIVISIONS = ["open", "closed"]
 
 SYSTEM_PATH = {
-    "v2.0": "{division}/{submitter}/systems/{system}.json",
-    "v3.0": "{division}/{submitter}/systems/{system}.json",
-    "default": "{division}/{submitter}/systems/{system}.json",
+    "v2.0": "{division}/{submitter}/systems/{system}.yaml",
+    "v3.0": "{division}/{submitter}/systems/{system}.yaml",
+    "default": "{division}/{submitter}/systems/{system}.yaml",
+}
+
+PARSER_MAP = {
+    "System": YamlParser,
+    "Summary": JSONParser,
+    "Metadata": JSONParser,
+    "default": JSONParser
+}
+
+DATAGEN_REQUIRED_FILES = {
+    "v2.0": ["training_datagen.stdout.log", "training_datagen.stderr.log", "*output.json", "*per_epoch_stats.json", "*summary.json", "dlio.log"],
+    "v3.0": ["training_datagen.stdout.log", "training_datagen.stderr.log", "*output.json", "*per_epoch_stats.json", "*summary.json", "dlio.log"],
+    "default": ["training_datagen.stdout.log", "training_datagen.stderr.log", "*output.json", "*per_epoch_stats.json", "*summary.json", "dlio.log"],
+}
+
+DATAGEN_REQUIRED_FOLDERS = {
+    "v2.0": ["dlio_config"],
+    "v3.0": ["dlio_config"],
+    "default": ["dlio_config"],
+}
+
+RUN_REQUIRED_FILES = {
+    "v2.0": ["training_run.stdout.log", "training_run.stderr.log", "*output.json", "*per_epoch_stats.json", "*summary.json", "dlio.log"],
+    "v3.0": ["training_run.stdout.log", "training_run.stderr.log", "*output.json", "*per_epoch_stats.json", "*summary.json", "dlio.log"],
+    "default": ["training_run.stdout.log", "training_datagen.stderr.log", "*output.json", "*per_epoch_stats.json", "*summary.json", "dlio.log"],
+}
+
+RUN_REQUIRED_FOLDERS = {
+    "v2.0": ["dlio_config"],
+    "v3.0": ["dlio_config"],
+    "default": ["dlio_config"],
+}
+
+CHECKPOINT_REQUIRED_FILES = {
+    "v2.0": ["training_run.stdout.log", "training_run.stderr.log", "*output.json", "*per_epoch_stats.json", "*summary.json", "dlio.log"],
+    "v3.0": ["training_run.stdout.log", "training_run.stderr.log", "*output.json", "*per_epoch_stats.json", "*summary.json", "dlio.log"],
+    "default": ["training_run.stdout.log", "training_datagen.stderr.log", "*output.json", "*per_epoch_stats.json", "*summary.json", "dlio.log"],
+}
+
+CHECKPOINT_REQUIRED_FOLDERS = {
+    "v2.0": ["dlio_config"],
+    "v3.0": ["dlio_config"],
+    "default": ["dlio_config"],
 }
